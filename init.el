3a8cd45fd25a92e009ed15ea396790c73257a8a5
Charles Haynes <haynes+git@biz.fifth-mountain.com>
2014-11-07 00:28:49 +1100

    start rebuilding emacs customizations

---
+++ init.el
@@ -0,0 +1,11 @@
+(setq load-path
+      (cons (expand-file-name "~/.emacs.d/") load-path))
+
+(setq custom-file (expand-file-name "~/.emacs.d/emacs-custom.el"))
+
+(require 'emacs-custom)
+
+(require
+ 'cask "~/.emacs.d/.cask/24.4.1/elpa/cask-20141026.1127/cask.el"
+ )
+(cask-initialize)
