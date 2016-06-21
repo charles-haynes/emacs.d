c85a32a31fd53f70d34b2830ace3533ecfb0457f
Charles Haynes <ceh+github@ceh.bz>
2016-06-21 11:18:30 +1000

    update comment about bootstrap, put in (commented out) code to use melpa

--- init.el
+++ init.el
@@ -2,11 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
+
+;; (require 'package)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/"))
+;; (when (< emacs-major-version 24)
+;;   ;; For important compatibility libraries like cl-lib
+;;   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/my-lisp"))
 
 ;; in a new installation do
-; brew install cask
-; cask install
+; bash -x go.sh
 
 (require 'cask "~/.emacs.d/.cask/cask.el")
 (cask-initialize)
