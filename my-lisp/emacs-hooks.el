19893b6044e0ab1198c3646d17c4c368a2c5fab0
Charles Haynes <ceh+gitlab@ceh.bz>
2017-03-24 15:15:09 +1100

    use local eslint if exists, tweak go mode

--- my-lisp/emacs-hooks.el
+++ my-lisp/emacs-hooks.el
@@ -12,7 +12,23 @@
 (eval-after-load 'css-mode
   '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
 
+;;; flycheck (from http://emacs.stackexchange.com/a/21207/14066)
+(defun my/use-eslint-from-node-modules ()
+  "Look for eslint in current dir and all parents."
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+;;; go-mode
 (defun my-go-mode-hook ()
+  "Various tweaks to make go-mode nicer."
   ; make go compile do a go install
   (lambda () (set (make-local-variable 'compile-command) "go install -x -v"))
   ; Call Gofmt before saving
